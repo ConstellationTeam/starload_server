17b06f6e8a7268978e84178f4eac6df426c73d01
Chanhuyk Lee <cksgur0612@dgsw.hs.kr>
2022-09-21 16:29:37 +0900

    testing server spring boot

---
+++ starload/src/main/java/com/example/starload/StarloadApplication.java
@@ -0,0 +1,13 @@
+package com.example.starload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarloadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarloadApplication.class, args);
+    }
+
+}
---
+++ starload/src/test/java/com/example/starload/StarloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.starload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarloadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
