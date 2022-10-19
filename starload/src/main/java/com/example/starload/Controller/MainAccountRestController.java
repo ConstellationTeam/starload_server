3e72be3f6ff1a78d1603e4b6b4e17ef943ec54f6
Chanhuyk Lee <cksgur0612@dgsw.hs.kr>
2022-10-19 21:10:05 +0900

    insert/select complete!! login complete!! more ready..

--- starload/src/main/java/com/example/starload/Controller/MainAccountRestController.java
+++ starload/src/main/java/com/example/starload/Controller/MainAccountRestController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import batang.common.Controller.CommonRestControllerPrototype;
 
 import java.io.IOException;
@@ -17,6 +18,9 @@ public class MainAccountRestController extends CommonRestControllerPrototype {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @PostMapping("/signup.do")
     @ModelAttribute()
     public ResponseEntity<?> addUser(@RequestBody User user) {
@@ -24,19 +28,27 @@ public class MainAccountRestController extends CommonRestControllerPrototype {
             @Override
             public Object execute() {
                 //LoggerFactory.getLogger(getClass()).info("이이이 : 성?공");
-                System.out.println(user.getUserName());
-                return userService.register(user);
+                System.out.println(user.toString());
+                userService.register(user);
+                return user;
             }
         });
     }
-    @PostMapping("/login.do")
-    public ResponseEntity<?> getUser(@RequestParam(value = "id") String data) {
-        System.out.println(data + ";");
+    @PostMapping("/signin.do")
+    public ResponseEntity<?> getUser(@RequestBody User user) {
+        System.out.println(user + ";");
 
         return controllerContext.execute(new RestControllerStrategy() {
+            User origin = userService.get(user.getUserIdentity());
             @Override
             public Object execute() {
-                return userService.get(data);
+                if(origin == null) {
+                    return "cannot find User["+ user.getUserIdentity() +"]!!";
+                } else if(passwordEncoder.matches(user.getUserPassword(), origin.getUserPassword())) {
+                    return origin;
+                } else {
+                    return "not match password!!";
+                }
             }
         });
     }
